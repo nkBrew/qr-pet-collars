083fcb41aa24c0b1362dd1afc965a8fc17d9d83c
Logan Olfert <logan@skillshark.com>
2023-09-10 18:18:54 -0600

    Hello world!

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react';
+import { Typography } from 'antd';
+
+export const App = () => {
+    return (
+        <Typography.Text>
+            Hello World!
+        </Typography.Text>
+    );
+};
