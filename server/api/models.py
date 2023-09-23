d116864122bb59216de9760dee73be29894b2c1d
Logan Olfert <logan@skillshark.com>
2023-09-22 20:07:09 -0600

    Made collar model

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Collar(models.Model):
+    pet_name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    weight = models.DecimalField(null=True, default=None, max_digits=8, decimal_places=2)
+    owner_name = models.CharField(max_length=100)
+    owner_email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
