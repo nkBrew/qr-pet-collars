f55fc8915ace5486cd5173adfbd8da47bf922dcf
Logan Olfert <logan@skillshark.com>
2023-09-22 22:11:22 -0600

    Added react query, starting to make view collar page

--- client/src/ViewCollar/ViewCollarPage.tsx
+++ client/src/ViewCollar/ViewCollarPage.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { useQuery } from 'react-query';
+import { Col, Row, Typography } from 'antd';
 
 export const ViewCollarPage = () => {
+    const params = useParams();
+
+    const collarId = Number(params.collarId);
+
+    const {
+        data: collar,
+        isFetching: isLoadingCollar,
+    } = useQuery({
+        queryKey: ['collar', collarId],
+        queryFn: () => axios({
+            method: 'get',
+            url: `/collar/${collarId}/`,
+        }),
+        enabled: true,
+        retry: false,
+        refetchOnWindowFocus: false,
+        staleTime: 60 * 1000,
+    });
+
+    if (isLoadingCollar) {
+        return (
+            <Row>
+                <Col>
+                    <Typography.Text>
+                        Loading...
+                    </Typography.Text>
+                </Col>
+            </Row>
+        );
+    }
+
+    if (!collar) {
+        return (
+            <Row>
+                <Col>
+                    <Typography.Text>
+                        Collar Not Found :(
+                    </Typography.Text>
+                </Col>
+            </Row>
+        );
+    }
+
     return (
-        <>View Collar</>
+        <Row justify={'center'}>
+            <Col flex={'60rem'}>
+                <Row gutter={[0, 24]}>
+                    <Col span={24}>
+                        <Typography.Text>
+                            Collar Found!
+                        </Typography.Text>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     );
 };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Typography } from 'antd';
 
 import './index.css';
@@ -11,16 +12,20 @@ import { ROUTES } from './routes';
 import Auth from './Auth';
 import { ViewCollarPage } from './ViewCollar/ViewCollarPage';
 
+const queryClient = new QueryClient();
+
 const container = document.getElementById('app-root')!
 const root = createRoot(container)
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path={ROUTES.login} element={<Auth/>}/>
-                <Route path={ROUTES.viewCollar} element={<ViewCollarPage/>}/>
-                <Route path={'*'} element={<Typography.Text>Page Not Found</Typography.Text>}/>
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={ROUTES.login} element={<Auth/>}/>
+                    <Route path={ROUTES.viewCollar} element={<ViewCollarPage/>}/>
+                    <Route path={'*'} element={<Typography.Text>Page Not Found</Typography.Text>}/>
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     </React.StrictMode>
 )
