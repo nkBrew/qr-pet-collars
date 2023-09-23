fae924dd21d46fd95766633b9fab4eb458eef251
goose <dylan@skillshark.com>
2023-09-23 11:41:47 -0600

    Display string pet name for admin

--- server/api/models.py
+++ server/api/models.py
@@ -11,3 +11,6 @@ class Collar(models.Model):
     phone_number = models.CharField(max_length=20)
     qr_code_id = models.IntegerField()
     is_missing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'name: {self.pet_name} QR code id:{self.qr_code_id}'
